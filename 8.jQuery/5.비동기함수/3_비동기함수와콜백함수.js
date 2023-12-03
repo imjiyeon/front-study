91e42e5a9fbc973ac2242fe9677311cd78a01cb9
yoyt777 <yoyt777@gmail.com>
2023-12-03 13:12:08 +0900

    a

--- 8.jQuery/5.비동기함수/3_비동기함수와콜백함수.js
+++ 8.jQuery/5.비동기함수/3_비동기함수와콜백함수.js
@@ -1,15 +1,13 @@
-/* 비동기함수를 콜백함수로 처리하기 */
 
-const result = true; // 예상결과
-const sec = 3;
+const result = true; // 서버에 데이터를 요청하고 받은 성공 여부
 
-setTimeout(function(){ //callback 함수
-    if(result){
+setTimeout(function () { // 콜백함수
+    if (result) {
         console.log('성공');
     } else {
         console.error('실패');
     }
-}, sec * 1000);
+}, 3000);
 
 /*
 setTimeout()와 같은 비동기 함수의 결과를 콜백 함수로 바로 처리하면,
--- 8.jQuery/5.비동기함수/5_promise.js
+++ 8.jQuery/5.비동기함수/5_promise.js
@@ -1,19 +1,18 @@
 /* 비동기함수를 promise로 처리하기 */
 
-const result = false;
-const sec = 3;
+const result = false; //요청성공여부
 
 // promise를 생성하면 resolve, reject 함수를 인자로 사용할수있음
 // resolve: 작업이 성공적으로 끝나면 결과데이터를 전달
 // reject: 문제가 발생하면 에러데이터를 전달 
 const promise = new Promise((resolve, reject) => { 
     setTimeout(() => {
-        if(result){
-            resolve('성공');
+        if(result){ //서버에 데이터를 요청하고 정상적으로 응답이 왔으면
+            resolve('성공'); //resolve함수를 호출하면서 데이터 전달
         } else {
-            reject('실패');
+            reject('실패'); //요청에 실패했으면 reject함수를 호출
         }
-    }, sec * 1000);
+    }, 3000);
 });
 
 //요청에 성공하면 then에 성공메세지를 전달하고 남은 작업을 처리
@@ -24,7 +23,7 @@ promise
     console.log(message);
 })
 .catch((error) => {
-    console.error(error);
+    console.log(error);
 });
 
 /*
