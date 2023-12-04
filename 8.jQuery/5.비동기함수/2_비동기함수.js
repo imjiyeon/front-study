69dad50596922c506daee9172a9aae15039b00cb
yoyt777 <yoyt777@gmail.com>
2023-12-04 19:06:56 +0900

    11

--- 8.jQuery/5.비동기함수/2_비동기함수.js
+++ 8.jQuery/5.비동기함수/2_비동기함수.js
@@ -2,7 +2,8 @@
 
 console.log('시작');
 
-setTimeout(function callback() { //3초를 대기하고 문자를 출력
+//타이머함수 인자: 실행할 함수(콜백함수), 지연시간(밀리세컨드) 
+setTimeout(function callback() { //3초를 대기하고 로그 출력
     console.log('timer 종료');
 }, 3000);
 
@@ -10,6 +11,5 @@ console.log('종료');
 
 //결과: 시작->종료->timer 종료
 
-//setTimeout는 비동기식으로 처리되는 함수이다
-//따라서 setTimeout()이 끝나지 않았는데도 '종료'가 출력된 것이다
-//만약 setTimeout이 동기식으로 처리된다면 'timer 종료'->'종료' 순으로 출력될 것이다
+//타이머함수는 비동기식으로 처리되기 때문에 '종료'가 먼저 출력되었다.
+//만약 타이머함수가 동기식으로 처리된다면 '시작 -> timer 종료-> 종료' 순으로 출력될 것이다
