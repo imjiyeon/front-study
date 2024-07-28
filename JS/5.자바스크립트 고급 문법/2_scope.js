a0d6dd700cb4834c16a7477b3b7f8578b29a0f74
임지연 <yoyt12@naver.com>
2024-07-28 14:14:24 +0900

    ㅇㅇㅇ

--- JS/5.자바스크립트 고급 문법/2_scope.js
+++ JS/5.자바스크립트 고급 문법/2_scope.js
@@ -6,9 +6,11 @@ function myFunction() {
 
 myFunction();
 
+//carName는 함수 안에서만 사용할 수 있음
 //console.log(carName); //에러남
 
 //global scope: 함수 밖에 선언된 변수는 어디서나 사용 가능
+//carName2는 어디서나 사용할 수 있음
 var carName2 = "기아";
 
 function myFunction2() {
