77e393d7ba0c6efa26b8cc7798c820951b9c1c90
임지연 <yoyt12@naver.com>
2024-08-13 20:29:52 +0900

    ㄴㅇㄴㅇ

--- JS/5.자바스크립트 고급 문법/9_object_destruct.js
+++ JS/5.자바스크립트 고급 문법/9_object_destruct.js
@@ -9,10 +9,20 @@ let person = {
   country: "USA",
 };
 
-//object에서 해당키만 추출해서 새로운 object 생성
-var { firstName, lastName } = person;
+//객체에서 해당키만 추출해서 새로운 객체 생성
+//객체는 분해시 {} 중괄호 사용
+let { firstName, lastName } = person;
 console.log(firstName);
 console.log(lastName);
+
+// 객체에 없는 키 사용하면 분해가 제대로 안됨
+// let { firstName, aa } = person;
+// console.log(aa); //aa는 undefined
+
+// 이름을 바꿔서 대입하고 싶으면 이런식으로 작성
+let { firstName: fName, lastName: lName } = person;
+console.log(fName);
+console.log(lName);
 
 //전달받은 object에서 필요한 데이터만 추출
 function displayFullName({ firstName, lastName }) {
