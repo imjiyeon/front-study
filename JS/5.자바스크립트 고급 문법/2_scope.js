8a0e5a86e564d55d5d7854585b7220490f7124fe
임지연 <yoyt12@naver.com>
2024-08-25 12:37:05 +0900

    ㄴㅇㄴㅇ

--- JS/5.자바스크립트 고급 문법/2_scope.js
+++ JS/5.자바스크립트 고급 문법/2_scope.js
@@ -1,20 +1,20 @@
-//local scope: 함수 내에 선언된 변수는 함수 안에서만 사용 가능
+//지역스코프(local scope):
+//함수 안에 선언된 변수는 함수 안에서만 사용 가능
 function myFunction() {
-  let carName = "볼보";
-  console.log(carName);
+  let lovalVar = "볼보";
+  console.log(lovalVar); //lovalVar은 함수가 끝나면 메모리에 사라짐
 }
-
 myFunction();
 
 //carName는 함수 안에서만 사용할 수 있음
 //console.log(carName); //에러남
 
-//global scope: 함수 밖에 선언된 변수는 어디서나 사용 가능
-//carName2는 어디서나 사용할 수 있음
-var carName2 = "기아";
+//전역스코프(global scope):
+//전역컨텍스트, 함수 밖에 선언된 변수는 어디서나 사용 가능
+var globalVar = "기아"; // 스크립트 내 모든 함수와 블록에서 접근 가능
 
 function myFunction2() {
-  console.log(carName2);
+  console.log(globalVar);
 }
 
 myFunction2();
