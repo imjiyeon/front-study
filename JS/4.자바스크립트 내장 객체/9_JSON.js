8ee74e3ff4faf553ea4d118b47a5e5585fffa257
imjiyeon <yoyt12@naver.com>
2024-08-26 11:12:51 +0900

    ㅇㅇㅇ

--- JS/4.자바스크립트 내장 객체/9_JSON.js
+++ JS/4.자바스크립트 내장 객체/9_JSON.js
@@ -27,3 +27,8 @@ console.log(typeof text); //string
 //JSON 문자열을 자바스크립트 객체로 변환
 var obj = JSON.parse(text);
 console.log(typeof obj); //object
+
+//JSON을 변환해야하는 이유
+//외부와 통신할 때(예: API 요청), 데이터를 주고받기 위해 JSON형식의 문자열을 사용함
+//하지만 문자열은 자바스크립트에서 바로 사용하기 어려움
+//JSON 문자열을 자바스크립트 객체로 변환하여 프로퍼티를 꺼내 쓸 수 있음
