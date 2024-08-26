72b01d3987b1e64ecf46cc296fed2b93cfe8028c
imjiyeon <yoyt12@naver.com>
2024-08-26 16:27:52 +0900

    cbvcb

--- JS/4.자바스크립트 내장 객체/9_JSON.js
+++ JS/4.자바스크립트 내장 객체/9_JSON.js
@@ -10,10 +10,12 @@ var data = {
     { firstName: "Peter", lastName: "Jones" },
   ],
 };
+console.log(data);
 console.log(typeof data); //object
 
 //자바스크립트 객체를 JSON 형식의 문자열로 변환
 var stringValue = JSON.stringify(data);
+console.log(stringValue);
 console.log(typeof stringValue); //string
 
 //json
@@ -32,3 +34,5 @@ console.log(typeof obj); //object
 //외부와 통신할 때(예: API 요청), 데이터를 주고받기 위해 JSON형식의 문자열을 사용함
 //하지만 문자열은 자바스크립트에서 바로 사용하기 어려움
 //JSON 문자열을 자바스크립트 객체로 변환하여 프로퍼티를 꺼내 쓸 수 있음
+
+console.log(obj.employees[0]);
