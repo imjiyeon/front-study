32f0b3d3ee5f176227b93e0f28fd3327ea1201a8
imjiyeon <yoyt12@naver.com>
2024-08-26 10:03:59 +0900

    fgfg

--- JS/4.자바스크립트 내장 객체/7_Map.js
+++ JS/4.자바스크립트 내장 객체/7_Map.js
@@ -6,26 +6,27 @@ let userMap = new Map();
 //새로운 데이터 추가
 userMap.set("name", "홍길동");
 userMap.set("email", "abc@email.com");
-userMap.set("phone", "000-0000-0000");
+userMap.set("phone", 100);
+console.log(userMap);
 
-//데이터 수정
-userMap.set("phone", "012-3456-7899");
+// //데이터 수정
+// // userMap.set("phone", "012-3456-7899");
 
-// Map의 크기 확인
-console.log(userMap.size); // 3
+// // Map의 크기 확인
+// console.log(userMap.size); // 3
 
-// 특정 키의 데이터 가져오기
-console.log(userMap.get("name")); // "홍길동"
+// // 특정 키의 데이터 가져오기
+// console.log(userMap.get("name")); // "홍길동"
 
-// 특정 키가 존재하는지 확인
-console.log(userMap.has("name")); // true
+// // 특정 키가 존재하는지 확인
+// console.log(userMap.has("name")); // true
 
-// 특정 데이터 삭제
-userMap.delete("name");
+// // 특정 데이터 삭제
+// userMap.delete("name");
 
 // Map에 저장된 모든 데이터 출력
-userMap.forEach(function (item) {
-  console.log(item);
+userMap.forEach(function (a, b) {
+  console.log(a, b);
 });
 
 // for of 반복문으로 map 순회하기
