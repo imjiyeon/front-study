7a7ae2ae3c3c05f216dfb2206659d06f112dcbad
imjiyeon <yoyt12@naver.com>
2024-08-26 17:29:13 +0900

    SDS

--- JS/5.자바스크립트 고급 문법/7_객체 리터럴.js
+++ JS/5.자바스크립트 고급 문법/7_객체 리터럴.js
@@ -1,12 +1,12 @@
 // 변수를 객체의 키로 사용하기
-var type = "student";
+var type = "name";
 
 var score = {
-  [type]: "John",
+  [type]: "John", // name: "John"
   score: 95,
 };
 
 // 객체의 키를 동적으로 추가하기
-score["grade"] = 1;
+score["grade"] = "A"; // score.grade = A 와 같음
 
 console.log("");
