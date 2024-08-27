e2d9fcba5de5c221f891c7e03064e62bd8713fbc
임지연 <yoyt12@naver.com>
2024-08-27 22:20:11 +0900

    ㅇㄹㅇ

--- JS/5.자바스크립트 고급 문법/11_fetch api.js
+++ JS/5.자바스크립트 고급 문법/11_fetch api.js
@@ -1,21 +1,22 @@
+//*실제서버 연결해보기 (192.168.0.67)
+
 //fetch: 서버와 데이터를 주고받기 위한 API 호출 함수
-//fetch함수의 인자: 
+//fetch함수의 인자:
 //첫번째인자: 서버의 주소(URL)
 //두번째인자: 전송할 데이터와 HTTP 메소드 등을 포함하는 객체
-//THEN: 서버로부터 응답을 받아 처리하는 함수
 
 //GET - 1번 게시물 조회
-fetch("http://192.168.0.67:8080/board/read?no=4") //서버로 GET 요청
+http: fetch("http://localhost:8080/board/read?no=1") //서버로 GET 요청
   .then((response) => response.json()) //응답 데이터를 JSON으로 변환
   .then((json) => console.log(json)); //JSON 데이터 출력
 
-// // POST - 새로운 게시물 생성
+// POST - 새로운 게시물 생성
 // fetch("http://localhost:8080/board/register", {
 //   method: "POST", //POST 요청
 //   body: JSON.stringify({
 //     title: "가입인사",
 //     content: "안녕하세요~",
-//     writer: "user"
+//     writer: "user",
 //   }), //전송할 데이터
 //   headers: {
 //     "content-type": "application/json; charset=UTF-8", //헤더 설정
@@ -44,3 +45,6 @@ fetch("http://192.168.0.67:8080/board/read?no=4") //서버로 GET 요청
 //   method: "DELETE",
 // })
 // .then((response) => console.log(response.status));
+
+//fetch함수는 자체적으로 promise객체를 생성하여 반환한다
+//그래서 fetch 함수 뒤에 then 또는 catch함수를 이어서 사용할 수 있다
