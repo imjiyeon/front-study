d27c6c6deb0099ef65cfc7ecdb0ddb0a3743831f
imjiyeon <yoyt12@naver.com>
2024-08-27 17:04:19 +0900

    ㄴㅇㄴㅇ

--- JS/5.자바스크립트 고급 문법/10_fetch api.js
+++ JS/5.자바스크립트 고급 문법/10_fetch api.js
@@ -1,46 +1,46 @@
-//fetch: API를 호출하여 서버로부터 데이터를 받아오는 함수
-//method~header: API주소와 파라미터를 입력하는 부분
-//THEN: 서버로부터 응답데이터를 받는 부분
+//fetch: 서버와 데이터를 주고받기 위한 API 호출 함수
+//fetch함수의 인자: 
+//첫번째인자: 서버의 주소(URL)
+//두번째인자: 전송할 데이터와 HTTP 메소드 등을 포함하는 객체
+//THEN: 서버로부터 응답을 받아 처리하는 함수
 
 //GET - 1번 게시물 조회
-fetch("localhost:8080/board/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+fetch("http://192.168.0.67:8080/board/read?no=4") //서버로 GET 요청
+  .then((response) => response.json()) //응답 데이터를 JSON으로 변환
+  .then((json) => console.log(json)); //JSON 데이터 출력
 
-//POST - 새로운 게시물 생성
-fetch("localhost:8080/board", {
-  method: "POST", //HTTP 요청 방법
-  body: JSON.stringify({
-    //전송할 데이터
-    title: "가입인사",
-    content: "안녕하세요~",
-    userId: "user1",
-  }),
-  headers: {
-    //헤더 값 정의
-    "content-type": "application/json; charset=UTF-8", //content-type 정의
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// // POST - 새로운 게시물 생성
+// fetch("http://localhost:8080/board/register", {
+//   method: "POST", //POST 요청
+//   body: JSON.stringify({
+//     title: "가입인사",
+//     content: "안녕하세요~",
+//     writer: "user"
+//   }), //전송할 데이터
+//   headers: {
+//     "content-type": "application/json; charset=UTF-8", //헤더 설정
+//   },
+// })
+//   .then((response) => response.text()) //응답이 단순한 텍스트일 때
+//   .then((json) => console.log(json));
 
 //PUT - 1번 게시물 수정
-fetch("localhost:8080/board/1", {
-  method: "PUT",
-  body: JSON.stringify({
-    id: 1,
-    title: "가입인사",
-    content: "반갑습니다~",
-    userId: "user1",
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch("http://localhost:8080/board/modify", {
+//   method: "PUT",
+//   body: JSON.stringify({
+//     id: 1,
+//     title: "가입인사",
+//     content: "반갑습니다~", //내용변경
+//     writer: "user",
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8",
+//   },
+// })
+// .then((response) => console.log(response.status)); //상태코드 확인
 
 //DELETE - 1번 게시물 삭제
-fetch("localhost:8080/board/1", {
-  method: "DELETE",
-});
+// fetch("http://localhost:8080/board/remove?no=1", {
+//   method: "DELETE",
+// })
+// .then((response) => console.log(response.status));
