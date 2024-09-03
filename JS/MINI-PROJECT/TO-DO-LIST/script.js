58b3bcfcc45553814c3e8323aecf6a66ef441f83
imjiyeon <yoyt12@naver.com>
2024-09-03 13:47:29 +0900

    a

---
+++ JS/MINI-PROJECT/TO-DO-LIST/script.js
@@ -0,0 +1,148 @@
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+
+  //더블클릭시 요소 삭제
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  // storageData?.complete(옵셔널체이닝)은 if(storageData && storageData.complete) 와 같음
+  // storageData 객체가 없으면 뒤에 있는 식을 실행하지 않음
+  // storageData 객체가 없으면 complete 속성을 꺼낼때 에러남
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
+};
+
+const keyCodeCheck = function () {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
+    createTodo();
+  }
+};
+
+// 모든 항목 삭제
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove(); //하나씩 삭제
+  }
+  saveItemsFn();
+};
+
+// todolist 만들고 로컬스토리지에 저장. 미완료/완료구분
+const saveItemsFn = function () {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  console.log(saveItems);
+
+  // === 값과 타입까지 정확히 비교하는 연산자
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
+
+const weatherDataActive = function ({ location, weather }) {
+  const weatherMainList = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Thunderstorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+
+  locationNameTag.textContent = location;
+  document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+  if (
+    !savedWeatherData ||
+    savedWeatherData?.location !== location ||
+    savedWeatherData?.weather !== weather
+  ) {
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+const weatherSearch = function ({ latitude, longitude }) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API Key}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      const weatherData = {
+        location: json.name,
+        weather: json.weather[0].main,
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const accessToGeo = function ({ coords }) {
+  const { latitude, longitude } = coords;
+  // shorthand property
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
+askForLocation();
+if (savedWeatherData) {
+  weatherDataActive(savedWeatherData);
+}
