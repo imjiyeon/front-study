fd4d07b40a8e68c28a7bef5dcdd254ea96d0e5da
imjiyeon <yoyt12@naver.com>
2025-03-31 17:06:33 +0900

    ddd

--- JS/3.자바스크립트 기본 문법/연습문제/Quiz6.js
+++ JS/3.자바스크립트 기본 문법/연습문제/Quiz6.js
@@ -1,10 +1,14 @@
 // 배열에 있는 모든 숫자를 더해서 합를 구하세요
 
+// 배열 생성
 const arr = [1, 'aa', true, 5, 10];
+// 합계를 저장할 변수
 let sum = 0;
 
+// 배열의 요소 중에서 number만 찾기
 for (let i = 0; i < arr.length; i++) {
 
+    // typeof 함수를 사용해서 tpye이 number인 요소 찾기
     let type = typeof arr[i];
     console.log(arr[i] + '의 타입은 ' + type + ' 입니다');
 
@@ -13,4 +17,4 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
-console.log('합계는 ' + sum + ' 입니다!'); // 16
+console.log('합계:', sum); // 16
